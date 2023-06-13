acf6d5defaf9fac0af86971f86f30d8a76d94781
maruf-ux <bafmarufulislam@gmail.com>
2023-06-13 22:40:45 +0600

    add slider properly

--- js/script.js
+++ js/script.js
@@ -9,3 +9,25 @@ jQuery(document).ready(function($) {
         time: 1000
     });
 });
+//  Slider
+$('.owl-carousel').owlCarousel({
+    autoplay: true,
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:4,
+            nav:true,
+            loop:false
+        }
+    }
+})
